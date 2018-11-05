4b9844f7632291254d72b19ae3e52fbee382c7a6
Bounds <44077291+Bounds97@users.noreply.github.com>
2018-11-05 10:23:27 +0800

    Update Env.java

--- src/main/java/config/Env.java
+++ src/main/java/config/Env.java
@@ -8,9 +8,9 @@ public class Env {
 	/**
      * 企业应用接入秘钥相关
      */
-	public static final String CORP_ID = "ding745efbea582c401835c2f4657eb6378f";
-	public static final String CORP_SECRET = "xGa2TyCoDxCerUr0vckiCp3jBHMr_dEupxQPCqjoOvQnQV0XS0CnpO0wOj-r7bpP";
+	public static final String CORP_ID = "xxxxxxxxxxxxxxxxxxxx";
+	public static final String CORP_SECRET = "xxxxxxxxxxxxxxxxxxxxxx";
 	public static final String SSO_Secret = "";
-	public static final String AGENTID = "199930233";
+	public static final String AGENTID = "";
 	public static final String OAPI_HOST = "https://oapi.dingtalk.com";
 }
